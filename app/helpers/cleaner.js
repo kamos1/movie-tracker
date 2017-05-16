d62d3b557a17050a083de74e90328fb39eca824d
kamos1 <keji.amos@gmail.com>
2017-05-15 21:53:53 -0600

    fix gitignore

---
+++ app/helpers/cleaner.js
@@ -0,0 +1,13 @@
+export const cleaner = (data) => {
+  return data.results.reduce((acc, val) => {
+    if(!acc[val.title]) {
+      acc[val.title] = {
+      title: val.title,
+      synopsis: val.overview,
+      poster: `https://image.tmdb.org/t/p/w500${val.poster_path}`,
+      id: val.id
+      }
+    }
+    return acc;
+  },{})
+}
