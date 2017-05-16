f89cf8d832d2ddb27646a9751bfa5d4aefe71eb4
Daniel Bucket <danieljbucket@gmail.com>
2017-05-16 16:09:02 -0600

    rendered individual cards to screen. go team

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,4 +1,6 @@
+import { Route, Link, Switch } from 'react-router-dom';
 import React, { Component } from 'react';
+import MovieContainer from './containers/MovieContainer';
 
 export default class App extends Component {
   constructor() {
@@ -9,6 +11,11 @@ export default class App extends Component {
     return (
       <div>
         <h1>Movie Watcher</h1>
+        <Route exact path="/" render={ (loction, history, match) => {
+          return (
+            <MovieContainer />
+          )
+        }} />
       </div>
     )
   }
