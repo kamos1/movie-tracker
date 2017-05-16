4ef0860688512b7c9f6f67e60c6640eefd435bdc
kamos1 <keji.amos@gmail.com>
2017-05-16 12:05:41 -0600

    create actions and reducers

--- app/index.js
+++ app/index.js
@@ -2,5 +2,11 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import MovieIndex from './components/movieIndex';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers/index'
+
+const store = createStore(rootReducer, devTools);
+
 
 ReactDOM.render(<App/>, document.getElementById('main'))
